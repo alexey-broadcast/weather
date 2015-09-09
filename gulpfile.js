1ba46c75c8a2aa30413c55d1deeeff53844a624a
Alex Lebedev <alexey.broadcast@gmail.com>
2015-09-09 23:45:45 +0300

    [+] Initial animation of smaller circles

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var connect         = require('gulp-connect');
 var sass            = require('gulp-sass');
 var autoprefixer    = require('gulp-autoprefixer');
 var minifyCss       = require('gulp-minify-css');
+var es6transpiler   = require('gulp-es6-transpiler');
+var uglify          = require('gulp-uglify');
+var traceur         = require('gulp-traceur');
+var plumber         = require('gulp-plumber');
 
 
 // ============== CONFIG ==============
@@ -17,9 +21,11 @@ cfg.src.dir = './src/';
 cfg.src.scssPattern = cfg.src.dir + 'scss/*.scss';
 cfg.src.scssDir = cfg.src.dir + 'scss/';
 cfg.src.htmlPattern = cfg.src.dir + '*.html';
+cfg.src.jsPattern = cfg.src.dir + 'js/*.js';
 
 cfg.build.dir = './build/';
 cfg.build.cssDir = cfg.build.dir + 'css/';
+cfg.build.jsDir = cfg.build.dir + 'js/';
 
 // ============== MAIN ==============
 
@@ -37,6 +43,7 @@ gulp.task('connect', function () {
 gulp.task('sass', function () {
     console.log('gulp: sass');
     gulp.src(cfg.src.scssPattern)
+        .pipe(plumber())
         .pipe(sass({
             includePaths: [cfg.src.scssDir]
         }))
@@ -47,10 +54,23 @@ gulp.task('sass', function () {
 });
 
 
+// js
+gulp.task('js', function () {
+    console.log('gulp: js');
+    gulp.src(cfg.src.jsPattern)
+        .pipe(plumber())
+        .pipe(traceur())
+        //.pipe(uglify())
+        .pipe(gulp.dest(cfg.build.jsDir))
+        .pipe(connect.reload());
+});
+
+
 // html
 gulp.task('html', function () {
     console.log('gulp: html');
     gulp.src(cfg.src.htmlPattern)
+        .pipe(plumber())
         .pipe(gulp.dest(cfg.build.dir))
         .pipe(connect.reload());
 });
@@ -60,8 +80,9 @@ gulp.task('html', function () {
 gulp.task('watch', function () {
     gulp.watch(cfg.src.scssPattern, ['sass']);
     gulp.watch(cfg.src.htmlPattern, ['html']);
+    gulp.watch(cfg.src.jsPattern, ['js']);
 });
 
 
 // default
-gulp.task('default', ['connect', 'sass', 'html', 'watch']);
+gulp.task('default', ['connect', 'sass', 'js', 'html', 'watch']);
---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    function initialAnimation() {
+        const id = '#div-time';
+        const circleSize = $('.main-circle').width();
+        const divTimeSize = $('.div-time').width();
+        const amend = (circleSize - divTimeSize) / 2;
+
+        const dur = 1200;
+        const count = 200;
+        const pi6 = -Math.PI / 6;
+        const startAngle = pi6;
+
+        function divTimeAnimation(n) {
+            const endAngle = pi6 * (4 + n);
+            const dAngle = (endAngle - startAngle) / count;
+
+            var int = setInterval(() => {
+                var angle = startAngle + dAngle * i++;
+                $('#div-time'+n).css('right', Math.round((Math.cos(angle) * circleSize) / 2 + amend) + 'px');
+                $('#div-time'+n).css('top', Math.round((Math.sin(angle) * circleSize) / 2 + amend) + 'px');
+                if(angle <= endAngle)
+                    clearInterval(int);
+            }, dur / count);
+        }
+
+        for(var i = 0; i < 5; ++i) {
+            divTimeAnimation(i);
+        }
+
+    }
+
+    initialAnimation();
+});
